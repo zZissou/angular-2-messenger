d7e4c156ce821f0c7dbc91699a2021e7101c8418
zZissou <kyle.edward.russell@gmail.com>
2017-03-01 00:31:00 -0800

    back end saving messages and http

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
 import { MessageInputComponent } from "./messages/message-input.component";
@@ -27,7 +28,13 @@ import { SigninComponent } from "./auth/signin.component";
     SignupComponent,
     SigninComponent
   ],
-  imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    routing,
+    ReactiveFormsModule,
+    HttpModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
