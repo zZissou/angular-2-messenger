69690aba78ed5603bbdfdb6cf1385b0723c428e8
zZissou <kyle.edward.russell@gmail.com>
2017-03-02 17:12:20 -0800

    user signin/logout

--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
+
+var User = require('../models/user');
 
 var Message = require('../models/message');
 
@@ -19,9 +22,31 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.use('/', function(req, res, next) {
+  jwt.verify(req.query.token, 'secret', function(err, decoded) {
+    if (err) {
+      return res.status(401).json({
+        title: 'Not Authenticated',
+        error: err
+      });
+    }
+    next();
+  })
+});
+
 router.post('/', function (req, res, next) {
+  var decoded = jwt.decode(req.query.token);
+  User.findById(decoded.user._id, function(err, user) {
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured',
+        error: err
+      });
+    }
+  });
   var message = new Message({
-    content: req.body.content
+    content: req.body.content,
+    user: user
   });
   message.save(function (err, result) {
     if (err) {
@@ -30,6 +55,8 @@ router.post('/', function (req, res, next) {
         error: err
       });
     }
+    user.messages.push(result);
+    user.save();
     res.status(201).json({
       message: 'Saved message',
       obj: result
@@ -93,7 +120,7 @@ router.delete('/:id', function(req, res, next) {
         obj: result
       });
     });
-  };
+  });
 });
 
 module.exports = router;
