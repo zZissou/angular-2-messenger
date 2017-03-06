b87f05811352e3ef804f0670b2608a506d0ac225
zZissou <kyle.edward.russell@gmail.com>
2017-03-06 13:34:17 -0800

    Message and User Error Handling

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -4,13 +4,14 @@ import 'rxjs/Rx';
 import { Observable } from "rxjs";
 
 import { Message } from "./message.model";
+import { ErrorService } from "../errors/error.service";
 
 @Injectable()
 export class MessageService {
   private messages: Message[] = [];
   messageIsEdit = new EventEmitter<Message>();
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private errorService: ErrorService) {}
 
   addMessage(message: Message) {
     const body = JSON.stringify(message);
@@ -28,7 +29,10 @@ export class MessageService {
         this.messages.push(message);
         return message;
       })
-      .catch((error: Response) => Observable.throw(error.json()));
+      .catch((error: Response) => {
+        this.errorService.handleError(error.json());
+        return Observable.throw(error.json());
+      });
   }
 
   getMessages() {
@@ -46,7 +50,10 @@ export class MessageService {
         this.messages = transformedMessages;
         return transformedMessages;
       })
-      .catch((error: Response) => Observable.throw(error.json()));
+      .catch((error: Response) => {
+        this.errorService.handleError(error.json());
+        return Observable.throw(error.json());
+      });
   }
 
   editMessage(message: Message) {
@@ -61,7 +68,10 @@ export class MessageService {
       : '';
     return this.http.patch('http://localhost:3000/message/' + message.messageId + token, body, {headers: headers})
       .map((response: Response) => response.json())
-      .catch((error: Response) => Observable.throw(error.json()));
+      .catch((error: Response) => {
+        this.errorService.handleError(error.json());
+        return Observable.throw(error.json());
+      });
   }
 
   deleteMessage(message: Message) {
@@ -71,6 +81,9 @@ export class MessageService {
       : '';
     return this.http.delete('http://localhost:3000/message/' + message.messageId + token)
       .map((response: Response) => response.json())
-      .catch((error: Response) => Observable.throw(error.json()));
+      .catch((error: Response) => {
+        this.errorService.handleError(error.json());
+        return Observable.throw(error.json());
+      });
   }
 }
