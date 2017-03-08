2ab2e2ad4af9d1b4006ae97b5bd8003c862a039f
zZissou <kyle.edward.russell@gmail.com>
2017-03-07 16:41:15 -0800

    updated package.json, AoT, and Auth lazy loading

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
+import { MessageModule } from "./messages/message.module";
 
 import { AppComponent } from "./app.component";
-import { MessageComponent } from "./messages/message.component";
-import { MessageListComponent } from "./messages/message-list.component";
-import { MessageInputComponent } from "./messages/message-input.component";
-import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header/header.component";
 import { routing } from "./app.routing";
-import { LogoutComponent } from "./auth/logout.component";
-import { SignupComponent } from "./auth/signup.component";
-import { SigninComponent } from "./auth/signin.component";
 import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
@@ -21,23 +14,15 @@ import { ErrorService } from "./errors/error.service";
 @NgModule({
   declarations: [
     AppComponent,
-    MessageComponent,
-    MessageListComponent,
-    MessageInputComponent,
-    MessagesComponent,
     AuthenticationComponent,
     HeaderComponent,
-    LogoutComponent,
-    SignupComponent,
-    SigninComponent,
     ErrorComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     routing,
-    ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    MessageModule
   ],
   providers: [AuthService, ErrorService],
   bootstrap: [AppComponent]
